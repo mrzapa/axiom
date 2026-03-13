ee5dc1045b5493fa68ad5d7518d1254a41279acc
mrzapa <96554662+mrzapa@users.noreply.github.com>
2026-03-13 23:24:22 +0000

    Add engine query wrappers (#334)

--- axiom_app/engine/__init__.py
+++ axiom_app/engine/__init__.py
@@ -2,11 +2,25 @@
 
 from axiom_app.engine.indexing import IndexBuildRequest, IndexBuildResult, build_index
 from axiom_app.engine.index_registry import get_index, list_indexes
+from axiom_app.engine.querying import (
+    DirectQueryRequest,
+    DirectQueryResult,
+    RagQueryRequest,
+    RagQueryResult,
+    query_direct,
+    query_rag,
+)
 
 __all__ = [
+    "DirectQueryRequest",
+    "DirectQueryResult",
     "IndexBuildRequest",
     "IndexBuildResult",
+    "RagQueryRequest",
+    "RagQueryResult",
     "build_index",
     "get_index",
     "list_indexes",
+    "query_direct",
+    "query_rag",
 ]
